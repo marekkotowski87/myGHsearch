f584a9a5bc4402e61e29a1f02b269ce589079e36
Marek Kotowski <marek.kotowski87@gmail.com>
2018-12-27 23:54:45 +0100

    Fetching & Displaying repos

--- github.js
+++ github.js
@@ -2,6 +2,8 @@ class GitHub {
   constructor() {
     this.client_id = 'de3ac121f25c2d5de399';
     this.client_secret = '7a4be04933ea9c424e738735be1d0df1c1001f34';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
   }
 
   async getUser(user) {
@@ -11,10 +13,20 @@ class GitHub {
       }&client_secret=${this.client_secret}`
     );
 
-    const profileData = await profileResponse.json();
+    const repoResponse = await fetch(
+      `https://api.github.com/users/${user}/repos?per_page=${
+        this.repos_count
+      }&client_id=${this.client_id}&sort=${this.repos_sort}&client_secret=${
+        this.client_secret
+      }`
+    );
+
+    const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
 
     return {
-      profile: profileData
+      profile,
+      repos
     };
   }
 }
